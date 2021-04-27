06e1f372de62ba3d4e249711f7f60bb1177d9ba4
ShookLyngs <shook-lyngs@qq.com>
2021-04-28 00:35:33 +0800

    update(component/confirm).

--- src/components/confirm/confirm.vue
+++ src/components/confirm/confirm.vue
@@ -1,10 +1,54 @@
 <template>
-
+  <modal-dialog :show="actualShow" v-on="$attrs">
+    <div>content</div>
+  </modal-dialog>
 </template>
 
 <script>
+  // Functions
+  // import { h } from 'vue';
+  import { useModel } from '@/hooks/use-model';
+  // Components
+  import ModalDialog from '@/components/modal-dialog.vue';
+
   export default {
-    name: 'confirm'
+    name: 'confirm',
+    components: {
+      ModalDialog,
+    },
+    props: {
+      show: {
+        type: Boolean,
+        default: false,
+      }
+    },
+    emits: ['update:show'],
+    setup(props, { emit }) {
+      const actualShow = useModel(
+        () => props.show,
+        (value) => emit('update:show', value)
+      );
+      function open() {
+        actualShow.value = true;
+      }
+      function close() {
+        actualShow.value = false;
+      }
+
+      return {
+        actualShow,
+        open,
+        close,
+      };
+    },
+    /*render() {
+      const slot = this.$slots.default;
+
+      return h(ModalDialog, {
+        show: this.actualShow,
+        ...this.$attrs,
+      }, slot ?? this.);
+    },*/
   };
 </script>
 
