f87a7258f7dd7ff7ed63f7da83916a3fe6df13ae
ShookLyngs <shook-lyngs@qq.com>
2021-06-07 00:10:27 +0800

    add(component/dropdown).

---
+++ src/components/dropdown.vue
@@ -0,0 +1,40 @@
+<template>
+  <popper
+    content-reachable
+    :offset="0"
+    :trigger="trigger"
+    :placement="placement"
+  >
+    <slot />
+
+    <template #content>
+      <slot name="content" />
+    </template>
+  </popper>
+</template>
+
+<script>
+  // Components
+  import Popper from '@/components/popper';
+
+  export default {
+    name: 'dropdown',
+    components: {
+      Popper,
+    },
+    props: {
+      trigger: {
+        type: String,
+        default: 'hover',
+      },
+      placement: {
+        type: String,
+        default: 'bottom',
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
